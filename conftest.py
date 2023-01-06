4372cd47bcc3cf8faee4bbe33b9e9b16c9933c49
macnev2013 <macnev2013@gmail.com>
2023-01-06 13:29:10 +0530

    debugging prints

--- conftest.py
+++ conftest.py
@@ -40,10 +40,13 @@ class GoItem(pytest.Item):
         service_path = dirname(Path(*relpath(self.path).split(os.sep)[1:]))
         service = service_path.split(os.sep)[-1]
 
-        return_code, stdout = build_test_bin(service=service, tf_root_path=tf_root_path)
-        if return_code != 0:
-            raise GoException(returncode=return_code, stdout=stdout)
-
+        # return_code, stdout = build_test_bin(service=service, tf_root_path=tf_root_path)
+        # if return_code != 0:
+        #     raise GoException(returncode=return_code, stdout=stdout)
+        return_code, stdout = execute_command(["ls -al"], cwd=tf_root_path)
+        print("---> return_code", return_code, stdout)
+        return_code, stdout = execute_command(["pwd"], cwd=tf_root_path)
+        print("---> pwd", return_code, stdout)
         env = dict(os.environ)
         env.update({
             'TF_ACC': '1',
