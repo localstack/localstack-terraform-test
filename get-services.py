43de7ae4a084ab59078ab770cbc1d14b05689944
macnev2013 <macnev2013@gmail.com>
2023-01-10 17:46:56 +0530

    running tests for all services

--- get-services.py
+++ get-services.py
@@ -2,25 +2,9 @@ import json
 import os
 
 blacklist_services = ["controltower"]
-whitelist_services = ["s3"]
 
 services = []
 for service in os.listdir('terraform-provider-aws/internal/service'):
-    if service in whitelist_services:
+    if service not in blacklist_services:
         services.append(service)
-    # if service not in blacklist_services:
-    #     services.append(service)
 print(json.dumps(services))
-
-
-# with open("tests/terraform/terraform-tests.yaml") as f:
-#     service_mapping = yaml.load(f, Loader=yaml.FullLoader)
-#     mapping = []
-#     for service, partition_or_tests in service_mapping.items():
-#         if isinstance(partition_or_tests, dict):
-#             partitions = list(partition_or_tests.keys())
-#             for partition in partitions:
-#                 mapping.append({"service": service, "partition": str(partition)})
-#         else:
-#             mapping.append({"service": service, "partition": None})
-#     print(json.dumps(mapping))
