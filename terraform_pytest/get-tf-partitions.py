48ed91653e68fa40060b1650e8be55b704da1c65
Bernhard Matyas <bernhard.matyas@localstack.cloud>
2023-03-28 13:53:17 +0200

    fix matrix, remove prints

--- terraform_pytest/get-tf-partitions.py
+++ terraform_pytest/get-tf-partitions.py
@@ -6,15 +6,11 @@ import yaml
 
 with open("terraform_pytest/service-partitions.yml") as f:
     service_partitions = yaml.load(f, Loader=yaml.FullLoader)
-    print(sys.argv[1])
     mapping = []
     for service in json.loads(sys.argv[1]):
         if service in service_partitions.keys():
             for partitioned_service, partitions in service_partitions.items():
-                print(f"Partitioned Service: {partitioned_service}")
-                print(f"Partitions: {partitions}")
                 partition_keys = list(partitions.keys())
-                print(partitions)
                 for partition in partition_keys:
                     mapping.append({"service": service, "partition": partition})
         else:
