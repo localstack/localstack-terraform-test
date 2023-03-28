f4aa3d42792c37089198f701f9cefdb194acf9b5
Bernhard Matyas <bernhard.matyas@localstack.cloud>
2023-03-28 14:27:16 +0200

    combine files, fix file naming

---
+++ terraform_pytest/get_services.py
@@ -0,0 +1,23 @@
+import json
+import sys
+
+from terraform_pytest.utils import get_services
+from terraform_pytest.get_tf_partitions import get_partitions,get_tests_for_partition
+
+service_partitions = []
+
+def main():
+    if len(sys.argv) > 1:
+        service = sys.argv[1]
+        services = get_services(service)
+        partitions = get_partitions(services)
+        for partition in partitions:
+            service_partitions.append(partition)
+        print(json.dumps(service_partitions))
+    else:
+        print("No service provided")
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ terraform_pytest/get_tf_partitions.py
@@ -0,0 +1,25 @@
+# Prints a JSON dict mapping the different partitions in the terraform-tests.yaml to their service
+import json
+import yaml
+
+
+def get_partitions(services: list[str]):
+    with open("terraform_pytest/service-partitions.yml") as f:
+        service_partitions = yaml.load(f, Loader=yaml.FullLoader)
+        mapping = []
+        for service in services:
+            if service in service_partitions.keys():
+                partition_keys = service_partitions[service].keys()
+                for partition in partition_keys:
+                    mapping.append({"service": service, "partition": partition})
+            else:
+                mapping.append({"service": service, "partition": "All"})
+        return mapping
+
+
+def get_tests_for_partition(service: str, partition: str):
+    with open("terraform_pytest/service-partitions.yml") as f:
+        service_partitions = yaml.load(f, Loader=yaml.FullLoader)
+        if service in service_partitions.keys():
+            return json.dumps(service_partitions[service][partition])
+
